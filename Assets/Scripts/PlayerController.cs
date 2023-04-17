30462470c64a9f7570b72eb9d878dbdccbbcf761
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-04-17 14:40:51 +0300

    Add Gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private CharacterController characterController;
     public float speed = 10f;
+
+    public float gravity = -9.81f;
+    private Vector3 gravityVector; 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -16,5 +19,8 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 moveVector = Input.GetAxis("Horizontal") * transform.right + Input.GetAxis("Vertical") * transform.forward;
         characterController.Move(moveVector * speed * Time.deltaTime);
+
+        gravityVector.y -= gravity * Time.deltaTime;
+        characterController.Move(gravityVector * Time.deltaTime);
     }
 }
